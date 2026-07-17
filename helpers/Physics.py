732842464ad8878215fce4a51ef6cb00ab24d140
Maxi Dechant <mdechant@ymail.com>
2026-07-17 13:59:40 +0200

    Added tests and computation of surface Hamiltonian.

--- helpers/Physics.py
+++ helpers/Physics.py
@@ -1,7 +1,82 @@
 import numpy as np
-import StatBasics
+import torch
+from helpers import StatBasics
+
+#If not stated stated otherwise, "electronLocations" always has dimensions [NUMBER_OF_BATCHES, NUMBER_OF_ELECTRONS, 3]
+
+def getProjectionMatrices(electronLocations: torch.Tensor):
+    #A projection matrix at point p with normal vector n is defined as P = I - nn^T.
+    normals = torch.nn.functional.normalize(electronLocations, p=2, dim=2)
+    return torch.eye(3).repeat(electronLocations.shape[0], electronLocations.shape[1], 1, 1) - torch.matmul(normals.unsqueeze(-1), normals.unsqueeze(-2))
+
+def getElectronPairForces(electronLocations: torch.Tensor):
+    numBatches = electronLocations.shape[0]
+    numElectrons = electronLocations.shape[1]
+    expandedElectronLocations = electronLocations.unsqueeze(2).expand(numBatches, numElectrons, numElectrons, 3).clone()
+    distanceMatrix = expandedElectronLocations - torch.transpose(expandedElectronLocations, dim0=1, dim1=2)
+    projectionMatrices = getProjectionMatrices(electronLocations).unsqueeze(2).expand(numBatches, numElectrons, numElectrons, 3, 3)
+    projectedDistancesMatrix = torch.linalg.norm(torch.matmul(projectionMatrices, distanceMatrix), ord=2, dim=3)
+    forceMatrix = 1. / projectedDistancesMatrix
+    summedForces = torch.sum(forceMatrix.diagonal(offset=0, dim1=-1, dim2=-2).zero_(), dim=(1,2))   #TODO: Check
+    return summedForces
+#TODO: Add tests for this.
+
+def getSphereCurvatureTerm(numBatches: int, numElectrons: int, sphereRadius: float):
+    #For the sphere: Mean curvature is M = 1/r and Gauss curvature K = 1/r^2. So (M^2-K) = 0.
+    #Keep this function only for future purposes when more general geometries will hopefully be available.
+    return torch.zeros([numBatches, numElectrons])
+
+def computeLocalEnergy(waveFunction: torch.nn.Module, electronLocations: torch.Tensor, sphereRadius: float, particleMass: float = 1.0):
+    #The Hamiltonian has three terms: The surface laplace term, the electrostatic term and the term for the manifolds curvature which is constant 0 for a sphere.
+    surfaceLaplacians = torch.div(getSurfaceLaplacian(waveFunction, electronLocations, sphereRadius), waveFunction.apply(electronLocations)) #TODO: Check division.
+    electrostaicForces = getElectronPairForces(electronLocations)
+    curvatureTerm = getSphereCurvatureTerm(electronLocations.shape[0], electronLocations.shape[1], sphereRadius)
+    return -1. / (2. * particleMass) * torch.sum(surfaceLaplacians, dim=1) + 0.5 * torch.sum(electrostaicForces, dim=1) + 0.5 * torch.sum(curvatureTerm, dim=1)
+
+def getExpectedValueFromWaveFunction():
+    pass
+
+def computeLocalEnergyGradient():
+    pass
 
 def estimateVariationalEnergy(waveFunction, numElectrons, sphereRadius):
-    numSamples = 10
-    electronSamples = StatBasics.sampleFromWaveFunction(waveFunction, numElectrons, sphereRadius)
+    batchSize = 16
+    electronSamples = torch.stack([StatBasics.sampleFromWaveFunction(waveFunction, numElectrons, sphereRadius) for _ in range(batchSize)])
+    surfaceLaplacians = getSurfaceLaplacian(waveFunction, electronSamples, sphereRadius)
+
+def getSurfaceGradient(waveFunction: torch.nn.Module, electronLocations: torch.Tensor):
+    normals = torch.nn.functional.normalize(electronLocations, p=2, dim=2)
+    waveNetworkParams = dict(waveFunction.named_parameters())
+
+    def callableWaveFunction(locations):
+        return torch.func.functional_call(waveFunction, waveNetworkParams, locations)
+
+    callableGradient = torch.func.jacrev(callableWaveFunction)
+    embeddedGradients = torch.func.vmap(callableGradient)(electronLocations)
+    surfaceGradients = embeddedGradients - torch.matmul(normals.unsqueeze(-2), embeddedGradients.unsqueeze(-1)).squeeze(-1) * normals
+    return surfaceGradients
+    
+def getSurfaceLaplacian(waveFunction: torch.nn.Module, electronLocations: torch.Tensor, sphereRadius: float):
+    normals = torch.nn.functional.normalize(electronLocations, p=2, dim=2)
+    waveNetworkParams = dict(waveFunction.named_parameters())
+
+    def callableWaveFunction(locations):
+        return torch.func.functional_call(waveFunction, waveNetworkParams, locations)
+    
+    def extractRelevantDerivatives(fullHessian: torch.Tensor):
+        helperIndex = torch.arange(fullHessian.shape[0])
+        return fullHessian[helperIndex,:,helperIndex,:]
+
+    #Use "torch.func.jacrev(torch.func.jacrev(...))" instead of "torch.func.hessian" since "hessian" throws weird deprecation warning.
+    callableHessian = torch.func.jacrev(torch.func.jacrev(callableWaveFunction))
+    completeHessian = torch.func.vmap(callableHessian)(electronLocations)
+    embeddedHessians = torch.func.vmap(extractRelevantDerivatives)(completeHessian)
+
+    embeddedLaplacians = torch.sum(torch.diagonal(embeddedHessians, offset=0, dim1=-1, dim2=-2), -1)
+    normalCorrection = torch.matmul(torch.matmul(normals.unsqueeze(-2), embeddedHessians), normals.unsqueeze(-1)).squeeze()
+    callableGradient = torch.func.jacrev(callableWaveFunction)
+    embeddedGradients = torch.func.vmap(callableGradient)(electronLocations)
+    curvatureCorrection = 2. / sphereRadius * torch.matmul(normals.unsqueeze(-2), embeddedGradients.unsqueeze(-1)).squeeze()
+
+    return embeddedLaplacians - normalCorrection - curvatureCorrection
     
--- playground/Tests.py
+++ playground/Tests.py
@@ -1,15 +1,48 @@
 import unittest
 import numpy as np
+import torch
 
 from helpers import LinAlgBasics as laBasics
+from helpers import Physics as phys
+
 
 class TestLinAlg(unittest.TestCase):
     def testPfaffian(self):
-        print("Testing computation of Pfaffian determinants...")
         testMatrixA = np.array([[0,2,3,4,5,6],[-2,0,1,0,1,0],[-3,-1,0,2,2,2],[-4,0,-2,0,0,1],[-5,-1,-2,0,0,-4],[-6,0,-2,-1,4,0]])
         exactPfaffianA = -34
         computedPfaffianA = laBasics.getPfaffian(testMatrixA)
         self.assertAlmostEqual(exactPfaffianA, computedPfaffianA)
+
+class SurfaceLaplacianTestNeuralNetwork(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return torch.sum(torch.sin(x[:,0]) + torch.exp(x[:,1]) * x[:,2]**2)
+
+class SurfaceGradientTest(unittest.TestCase):
+    def testSurfaceGradient(self):
+        testWaveFunction = SurfaceLaplacianTestNeuralNetwork()
+        unnormedEvaluationPoints = torch.tensor([[[1., 0., 0.], [1., 1., 0.]], [[1., 1., 1.], [0., -1., 0.]]])
+        testedRadii = [1., 2.]
+        exactSurfaceGradients = torch.tensor([[[[0., 0., 0.], [0.380122298537815, -0.380122298537815, 0.]], [[-0.324943065260291, -0.569084168251684, 0.894027233511974], [1., 0., 0.]]], [[[0., 0., 0.,], [0.0779718473826873, -0.0779718473826873, 0.]], [[-3.5834258344633, 0.243145785734844, 3.34028004872846], [1., 0., 0.]]]])
+        for rIndex, r in enumerate(testedRadii):
+            evaluationPoints = r * torch.nn.functional.normalize(unnormedEvaluationPoints, p=2, dim=2)
+            computedSurfaceGradients = phys.getSurfaceGradient(testWaveFunction, evaluationPoints)
+            discrepany = computedSurfaceGradients - exactSurfaceGradients[rIndex]
+            self.assertAlmostEqual(torch.linalg.norm(discrepany).item(), 0.0, 5)
+
+class SurfaceLaplacianTest(unittest.TestCase):
+    def testSurfaceLaplacian(self):
+        testWaveFunction = SurfaceLaplacianTestNeuralNetwork()
+        unnormedEvaluationPoints = torch.tensor([[[1., 0., 0.], [1., 1., 0.]], [[1., 1., 1.], [0., -1., 0.]]])
+        testedRadii = [1., 2.]
+        exactSurfaceLaplacians = torch.tensor([[[0.919395388263720, 2.65626327384969], [-2.99244262500077, 0.735758882342885]], [[2.41614683654714, 7.62234894051760], [-5.35053222399308, 0.270670566473225]]])
+        for rIndex, r in enumerate(testedRadii):
+            evaluationPoints = r * torch.nn.functional.normalize(unnormedEvaluationPoints, p=2, dim=2)
+            computedSurfaceLaplacians = phys.getSurfaceLaplacian(testWaveFunction, evaluationPoints, r)
+            discrepany = computedSurfaceLaplacians - exactSurfaceLaplacians[rIndex]
+            self.assertAlmostEqual(torch.linalg.norm(discrepany).item(), 0.0, 5)
 
 if __name__ == "__main__":
     unittest.main()
